674a3cd6241093ebfe00e6c4ad6080c810cde100
Stanly Shiyanovskiy <st-small@rambler.ru>
2018-12-22 12:45:04 +0200

    Initial Commit

---
+++ StretchyImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyImage
+//
+//  Created by Stanly Shiyanovskiy on 12/22/18.
+//  Copyright © 2018 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
